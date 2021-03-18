f2832e351f7636143dae49b5b6336fbd889aa2f7
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2021-03-18 14:09:58 +0000

    scenario001

--- src/gatling/simulations/uk/gov/hmcts/reform/CRD/performance/scenarios/Scenario1.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/CRD/performance/scenarios/Scenario1.scala
@@ -11,54 +11,11 @@ object Scenario1 {
 
   // Uploads caseworkers to CRD database
   .group("CRD_010_UploadCaseworkers") {
-    exec(http(requestName = "CRD_010_010_UploadCaseworkers1")
+    exec(http(requestName = "CRD_010_010_UploadCaseworkers")
       .post("/refdata/case-worker/upload-file")
       .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 1-10")
+      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 81-90.xlsx")
       .check(status.is(200)))
-
-    .exec(http(requestName = "CRD_010_020_UploadCaseworkers2")
-      .post("/refdata/case-worker/upload-file")
-      .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 11-20")
-      .check(status.is(200)))
-
-    .exec(http(requestName = "CRD_010_030_UploadCaseworkers3")
-      .post("/refdata/case-worker/upload-file")
-      .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 21-30")
-      .check(status.is(200)))
-
-    .exec(http(requestName = "CRD_010_040_UploadCaseworkers4")
-      .post("/refdata/case-worker/upload-file")
-      .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 31-40")
-      .check(status.is(200)))
-
-    .exec(http(requestName = "CRD_010_050_UploadCaseworkers5")
-      .post("/refdata/case-worker/upload-file")
-      .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 41-50")
-      .check(status.is(200)))
-
-    .exec(http(requestName = "CRD_010_060_UploadCaseworkers6")
-      .post("/refdata/case-worker/upload-file")
-      .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 51-60")
-      .check(status.is(200)))
-
-    .exec(http(requestName = "CRD_010_070_UploadCaseworkers7")
-      .post("/refdata/case-worker/upload-file")
-      .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 61-70")
-      .check(status.is(200)))
-
-    .exec(http(requestName = "CRD_010_080_UploadCaseworkers8")
-      .post("/refdata/case-worker/upload-file")
-      .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 71-80")
-      .check(status.is(200)))
-
   }
 
   .pause(thinkTime)
@@ -68,7 +25,7 @@ object Scenario1 {
     exec(http(requestName = "CRD_020_010_UploadRoleMappings")
       .post("/refdata/case-worker/upload-file")
       .headers(Environment.headers_auth)
-      .formUpload("file", "Service to IDAM Role Mapping IAC.xlsx")
+      .formUpload("file", "Service to IDAM Role Mapping-5.xlsx")
       .check(status.is(200)))
   }
 
