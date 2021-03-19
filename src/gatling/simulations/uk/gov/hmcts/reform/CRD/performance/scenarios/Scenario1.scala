e6db353bc5bed17ea74d6f46dd89abf009ef2c21
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2021-03-19 12:10:53 +0000

    scenario004

--- src/gatling/simulations/uk/gov/hmcts/reform/CRD/performance/scenarios/Scenario1.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/CRD/performance/scenarios/Scenario1.scala
@@ -14,7 +14,7 @@ object Scenario1 {
     exec(http(requestName = "CRD_010_010_UploadCaseworkers")
       .post("/refdata/case-worker/upload-file")
       .headers(Environment.headers_auth)
-      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 111-140.xlsx")
+      .formUpload("file", "Staff Data Upload Template V1.0.2-Amog (1) - 141-180.xlsx")
       .check(status.is(200)))
   }
 
